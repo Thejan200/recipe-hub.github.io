96d104a6f4512ab231e5d6174262357e12627555
Thejan200 <mahakumburathejan0823@gmail.com>
2026-09-04 11:03:43 +0530

    Move recipe author slightly lower inside recipe card

--- assets/js/video-loader.js
+++ assets/js/video-loader.js
@@ -33,6 +33,7 @@
       author.className='recipe-author';
       author.textContent='Author: Thejan Mahakumbura';
     }
+    author.style.marginTop='28px';
     if(author.parentNode===recipeBox&&recipeBox.lastElementChild===author) return;
     recipeBox.appendChild(author);
   }
